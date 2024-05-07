dd7728da5f005ffe386fb12da36e1d0cf5beae3f
nghiamai <nghiamaiquangtri@gmail.com>
2024-05-07 15:47:02 +0700

    start install swapgger

--- app/Repositories/AdminRepository.php
+++ app/Repositories/AdminRepository.php
@@ -7,11 +7,5 @@ use Illuminate\Support\Facades\DB;
 
 class AdminRepository
 {
-    private string $tableName = "admins";
-
-    function queryAllDoctors() : array
-    {
-        $sql = "SELECT * FROM $this->tableName";
-        return DB::select($sql);
-    }
+    
 }
